e05b6ad74704e187b925a56b0a409a8c7011fb20
KING <irealking2000@gmail.com>
2025-01-24 12:29:42 +0530

    Adding clerk

---
+++ src/components/Header.jsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Button, Navbar, TextInput } from "flowbite-react";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { dark, light } from "@clerk/themes";
+
+export default function Header() {
+  const { theme, setTheme } = useTheme();
+  const path = usePathname();
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        href={"/"}
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          NEXT
+        </span>
+        <span className="text-black dark:text-white">Blog</span>
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button color="gray" pill className="w-12 h-10 lg:hidden">
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          {theme === "light" ? <FaMoon /> : <FaSun />}
+        </Button>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              baseTheme: theme === "light" ? "light" : "dark",
+            }}
+          />
+        </SignedIn>
+        <SignedOut>
+          <Button gradientDuoTone="purpleToBlue" as={"div"}>
+            <SignInButton />
+          </Button>
+        </SignedOut>
+
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Link href={"/"}>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            Home
+          </Navbar.Link>
+        </Link>
+        <Link href={"/about"}>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            About
+          </Navbar.Link>
+        </Link>
+        <Link href={"/projects"}>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            Projects
+          </Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
